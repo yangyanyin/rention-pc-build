945167b9ad7023d14984987ee6e314a5ece30a8f
yangyanyin <yangyanyin@aliyun.com>
2021-01-28 20:04:14 +0800

    node

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const dotenv = require("dotenv")
+dotenv.config()
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+// 模板引擎
+app.engine('html', ejs.__express)
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, './build'))
+app.use(express.static('./build'))
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use((req, res) => {
+  res.status(404).send('Sorry cant find that!')
+})
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3000, '0.0.0.0', err => {
+  if (err) {
+    console.error('服务启动失败，重新启动。')
+  } else {
+    console.info(`服务启动成功:http://localhost:3000`)
+  }
+})
