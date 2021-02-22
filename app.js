42b1038732a49628388033570baf466e0629e271
yangyanyin <yangyanyin@aliyun.com>
2021-02-22 10:14:28 +0800

    server

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
-var routers = require('./routers/index')
+const routers = require('./routers/index')
 
 // 模板引擎
 app.engine('html', ejs.__express)
 app.set('view engine', 'html')
 app.set('views', path.join(__dirname, './build'))
 app.use(express.static('./build'))
+
 app.use(routers)
 app.use((req, res) => {
   res.status(404).send('Sorry cant find that!')
 })
 
-app.listen(3000, '0.0.0.0', err => {
+app.listen(3000, '0.0.0.0', (req, err) => {
   if (err) {
     console.error('服务启动失败，重新启动。')
   } else {
--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', function (req, res) {
+  console.log('首页')
+  res.render('html/index', { title: '首页' })
+})
+
+router.get('/c/:name', function (req, res) {
+  res.render('html/index', { title: '分类页面' })
+})
+
+router.get('/c/d/:name', function (req, res) {
+  res.render('html/index', { title: '详情页面' })
+})
+
 router.get('*', function (req, res) {
-  res.render('index')
+  res.render('html/index', { title: '全局页面' })
 })
 
 module.exports = router
